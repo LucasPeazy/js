d118a9420399432a860679d016bbdbe92c175e1f
Dmitrii <lucas.peazy@gmail.com>
2026-09-03 23:59:13 +0200

    Массивы: map, filter, reduce

--- day1.js
+++ day1.js
@@ -24,3 +24,23 @@ console.log(toUpperCase("hello"));  // должно быть HELLO
 console.log(findMax(20, 5, 12));  // должно быть 20
 console.log(findMax(5, 20, 12));  // должно быть 20
 console.log(findMax(12, 20, 5));  // должно быть 20
+
+
+const numbers = [5, 12, 8, 20, 3];
+
+function multBy2(numbers) {
+    return numbers.map(val=> val*2)
+}
+
+console.log(multBy2(numbers))
+
+function filterAbove10(numbers) {
+  return numbers.filter(val => val > 10)
+}
+console.log(filterAbove10(numbers));  // должно быть [12, 20]
+
+function sumAll(numbers) {
+  return numbers.reduce((acc, val) => acc + val, 0)
+}
+
+console.log(sumAll(numbers));  // должно быть 48 (5+12+8+20+3)
