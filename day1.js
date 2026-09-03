fe74df8c2e962574ac7b4c2fcc02c0e25e8aa271
Dmitrii <lucas.peazy@gmail.com>
2026-09-03 23:44:15 +0200

    День 2: функции isEven, findMax, toUpperCase

--- day1.js
+++ day1.js
@@ -1,9 +1,26 @@
-const name = "Dmitrii";
-let age = 20;
-
-function greet(name) {
-  return `Привет, ${name}!`;
+function isEven(num) {
+  return num % 2 === 0;
 }
 
-console.log(greet(name));
-console.log(age >= 18 ? "Взрослый" : "Ребёнок");
+console.log(isEven(4));  // должно быть true
+console.log(isEven(7));  // должно быть false
+
+function findMax(a, b, c) {
+    if ( a > b && a > c) {
+        return a
+    } else if (b > a && b > c) {
+        return b
+    } else {
+        return c
+    }
+}
+
+
+function toUpperCase(str) {
+  return str.toUpperCase()
+}
+
+console.log(toUpperCase("hello"));  // должно быть HELLO
+console.log(findMax(20, 5, 12));  // должно быть 20
+console.log(findMax(5, 20, 12));  // должно быть 20
+console.log(findMax(12, 20, 5));  // должно быть 20
